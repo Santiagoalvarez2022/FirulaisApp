fd9da781e41fa46e4216a14cc3d56f3f1c78269e
Santiagoalvarez2022 <santiagoalvarez290401@gmail.com>
2023-02-22 18:48:12 -0300

    cambios de estilos en el login

--- PI-Dogs-main/client/client/src/components/login/Login.jsx
+++ PI-Dogs-main/client/client/src/components/login/Login.jsx
@@ -1,22 +1,38 @@
 import React from "react";
 import style from './Login.module.css'
 import {Link} from "react-router-dom"
-
+import img from "./dogPresentacion.png"
 
 //hacer funcion que valla cambiando entre 4 o 5 imagense de fondo
 
 const Login = () =>{
     return( 
       <div className={style.all}>
-          <h1 className={style.title}> Firulais </h1>
-          
-        <div className={style.sub}>
-        <h3 className={style.subtitle}>Conocé a tus mascostas</h3>
-          <Link className={style.button} to= '/home' >
-            <div>
-              <h3>Entrar</h3> 
-            </div> 
-          </Link>
+        
+        <div className={style.conteiner_title} >
+          <h1 className={style.title}> FiruApp </h1>  
+        </div>
+
+        <div className={style.body}>
+          <div className={style.getIn}>     
+            <div className={style.sub}>
+              <div className={style.enunciado}>
+                <h3 className={style.subtitle}>Conocé a tus mascostas</h3>
+                <p>y aprendé más de ellas</p>
+              </div>
+              <div  className={style.button}>
+                <Link to= '/home' >
+                  <div>
+                    <h3>Entrar</h3> 
+                  </div> 
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <div className={style.imagen}>
+              <img src={img} alt="" />
+          </div>
         </div>
     
       </div>
