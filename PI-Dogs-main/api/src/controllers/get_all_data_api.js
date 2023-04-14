83b98c7e03b30daf3e2386f413b2765a1a4c25d2
Santiagoalvarez2022 <santiagoalvarez290401@gmail.com>
2023-04-14 18:52:29 -0300

    proteccion de data sensible

--- PI-Dogs-main/api/src/controllers/get_all_data_api.js
+++ PI-Dogs-main/api/src/controllers/get_all_data_api.js
@@ -1,10 +1,11 @@
 const  axios  = require("axios");
-const apikey = 'live_4BgY3mqBuJriRz3s7vIp5vNLBJ0bqZBIXqeJn5L0SP3AUHepxHvP01TiahMvVUPN'
+require("dotenv").config
+const{API_KEY} =process.env 
 
 //MODULARIZO EN UNA FUNCION LA PETICION A LA API y FILTRADO DE LAS PROPIEDADES NECESARIAS, PARA NO REPETIR CODIGO EN LOS CONTROLLERS
 
 const DATA_API = async () => {
-    const result = await  axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${apikey}`)
+    const result = await  axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
     
     //retorno un nuevo array con los datos filtrados y ordenados de manera similar a la base de datos
     return dogs = result.data.map(obj =>{
