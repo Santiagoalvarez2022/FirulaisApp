1817d86ae1f6ac19d35cec2bcd558e549a1d2823
Santiagoalvarez2022 <santiagoalvarez290401@gmail.com>
2023-04-14 18:44:29 -0300

    preparacion para el deployd

--- PI-Dogs-main/api/index.js
+++ PI-Dogs-main/api/index.js
@@ -1,12 +1,14 @@
-
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js'); 
 
+require("dotenv").config()//COMO HACEMOS ESTO EN LA RAIZ NO DEBEMOS REPETIRLO
+const {PORT} = process.env
+
 // Syncing all the models at alter
 conn.sync({alter: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     console.log("http://localhost:3001");
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening at',PORT); // eslint-disable-line no-console
   });
 });
     
