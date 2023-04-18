523d29c447605f3699f610cf113dfe5468bb64d1
Santiago Alvarez <santiagoalvarez290401@gmail.com>
2023-04-18 18:56:45 -0300

    Delete PI-Dogs-main directory

--- PI-Dogs-main/api/src/routes/routesTemperaments/route_getTemperaments.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router()
-const {handler_getTemperaments} = require('../../handlers/index')
-
-router.get("/",handler_getTemperaments)
-
-
-module.exports = router
