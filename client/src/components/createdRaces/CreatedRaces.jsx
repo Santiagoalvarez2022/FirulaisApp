ab5e35d0b5c743837692fb288b460aad0e193e5d
Santiagoalvarez2022 <santiagoalvarez290401@gmail.com>
2023-05-01 20:20:57 -0300

    modificacion de paginad onde estan las razas creadas y  estilos de la misma

--- client/src/components/createdRaces/CreatedRaces.jsx
+++ client/src/components/createdRaces/CreatedRaces.jsx
@@ -1,34 +1,72 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { get_createdRaces } from '../../redux/actions'
-import Card from '../card/Card'
-import style from "../card/Card.module.css"
+import {Link} from "react-router-dom"
+import imgBackup from "./dog.jpg"
+import style from "./CreatedRaces.module.css"
+import {FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-
-
+import { faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons"
+import { faHouse} from "@fortawesome/free-solid-svg-icons"
 
 export default function CreatedRaces() {
-    const dogs = useSelector(state=> state.createdRaces)
+    const dogs_complted = useSelector(state=> state.createdRaces)
     const dispatch = useDispatch()
     useEffect(()=>{
         dispatch(get_createdRaces())
     },[])
 
-    console.log(dogs);
-  return (
-    <div>
-        <h1>hola</h1>
+    const [form, SetForm] = useState("")
+    //manejador del input
+    const handlerForm = ({target}) =>{
+        let {value} = target;
+        
+        SetForm(value)
+    }
 
-        { dogs.length 
-            ?  dogs.map(dog=>{
-                const { name, id,  image, temperament, max ,min,type } = dog
-                return <div>
-                    <h1>{name}</h1>
-                    <img src={image} alt="" />
-                </div>        
+  return (
+    <div className={style.container}>
+        <div className={style.navbar}>
+                <div className={style.icon}>
+                    <Link to="/home">
+                        <FontAwesomeIcon className={style.icon_a} icon={faHouse} style={{  height:"2.1rem"}} /> 
+
+                    </Link>
+                </div>
+                <div className={style.searched}>
+
+                </div>
+                <form   className={style.SearchBar}>
+                    <input onChange={handlerForm} name="search" type="text"  value={form}/>
+                    <button> 
+                        <FontAwesomeIcon icon={faMagnifyingGlass} beat />
+                    </button>
+                </form>
+        </div>
+            
+
+        <div className={style.container_card}>
+            { dogs_complted.length 
+                ?  dogs_complted.map(dog=>{
+                    const { name, id, image,type } = dog
+
+                    return  <Link
+                    key={id} 
+                    to= {`/detail/${id}?type=${type}`} 
+                    className={style.card} > 
+                    <div className={style.imagen}>
+
+                    <img src={image||imgBackup} alt="" />
+                    </div>
+                    <div className={style.name}>
+                    <p>{name}</p>
+            
+                    </div>
+                </Link>
                     })
-            : null 
+                : null 
             } 
+        </div>    
     </div>
   )
 }
