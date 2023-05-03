d74399d24de8a78b48c4081f2c12a2183b539253
Santiagoalvarez2022 <santiagoalvarez290401@gmail.com>
2023-05-03 18:56:06 -0300

    correcion de formulario de creacion al selecionar los temperamentos, ahora es ncesario que coloque por lo menos un temperamento para crearse y no se pueden crear temperamentos personalizados

--- client/src/components/create/Create.jsx
+++ client/src/components/create/Create.jsx
@@ -4,18 +4,16 @@ import style from './Create.module.css'
 import { useDispatch, useSelector } from "react-redux";
 import { get_dogs,filter_temperament, post_dog,get_createdRaces } from "../../redux/actions";
 import image from './cargando.gif'
-import { Button, Modal, ModalHeader,ModalBody, ModalFooter } from "reactstrap";
 
 
 const validate = (form, dogs) =>{
   let error = {};
-  let{alturaMax, alturaMin, name, pesoMax, pesoMin, temperaments, vidaMax, vidaMin} = form;
+  let {alturaMax, alturaMin, name, pesoMax, pesoMin,  vidaMax, vidaMin} = form;
 
-  if(!name || !alturaMax || !alturaMin || !pesoMax || !pesoMin || !temperaments || !vidaMax || !vidaMin){
+  if(!name || !alturaMax || !alturaMin || !pesoMax || !pesoMin || !vidaMax || !vidaMin){
     error.vacio = "Todos los campos deben llenarse";
   }
-
-  if(form.name){
+  if(name){
     //cambiar para que consulte directamente a la base de datos y en la api para no crear duplicados 
     const find_dog = dogs.find((dog)=>  dog.name.trim().toLowerCase() === name.toLowerCase()  )
     if(find_dog){
@@ -26,18 +24,28 @@ const validate = (form, dogs) =>{
     } 
   }
 
-  if(!/^[0-9]+$/.test(alturaMax || alturaMin || pesoMax  || pesoMin || vidaMax || vidaMin)){
-    error.caracteres = "El valor de la Altura, Peso y Años de vida deben ser numeros enteros, no pueden contener -,+,.,etc"
-  } 
+  //evaluar solo si hay algo en los inputs
+  if (alturaMax || alturaMin) {
+    if(!/^[0-9]+$/.test(alturaMax || alturaMin)) error.altura ="Solo se aceptan numeros"
+    if(parseInt(alturaMax) < parseInt(alturaMin) || parseInt(alturaMax) === parseInt(alturaMin) ) error.altura = "La altura maxima no puede ser igual o menor a la altura minima"
+    if(parseInt(alturaMax) < 0 || parseInt(alturaMin) < 0) error.altura = "La altura no puede ser negativa"
+  }
 
-  if(parseInt(alturaMax) < parseInt(alturaMin) || parseInt(alturaMax) === parseInt(alturaMin) ) error.altura = "La altura maxima no puede ser igual o menor a la altura minima"
-  if(parseInt(alturaMax) < 0 || parseInt(alturaMin) < 0) error.altura = "La altura no puede ser negativa"
-  
-  if(parseInt(pesoMax) < parseInt(pesoMin) || parseInt(pesoMax) === parseInt(pesoMin) ) error.peso = "El peso maximo no puede ser igual o menor al peso minimo"
-  if(parseInt(pesoMax) < 0 || parseInt(pesoMin) < 0) error.peso = "El peso no puede ser negativo"
-  
-  if(parseInt(vidaMax) < parseInt(vidaMin) || parseInt(vidaMax) === parseInt(vidaMin) ) error.vida = "El promedio maximo no puede ser igual o menor al promedio minimo"
-  if(parseInt(vidaMax) < 0 || parseInt(vidaMin) < 0) error.vida = "El rango de vida no puede ser negativo"
+  if (pesoMax || pesoMin) { 
+    if(!/^[0-9]+$/.test(pesoMax || pesoMin)) error.peso ="Solo se aceptan numeros"
+    if(parseInt(pesoMax) < parseInt(pesoMin) || parseInt(pesoMax) === parseInt(pesoMin) ) error.peso = "El peso maximo no puede ser igual o menor al peso minimo"
+    if(parseInt(pesoMax) < 0 || parseInt(pesoMin) < 0) error.peso = "El peso no puede ser negativo"
+    
+  }
+    
+
+  if (vidaMax || vidaMin) { 
+    if(!/^[0-9]+$/.test(vidaMax || vidaMin)) error.vida ="Solo se aceptan numeros";
+    if(parseInt(vidaMax) < parseInt(vidaMin) || parseInt(vidaMax) === parseInt(vidaMin) ) error.vida = "El promedio maximo no puede ser igual o menor al promedio minimo";
+    if(parseInt(vidaMax) < 0 || parseInt(vidaMin) < 0) error.vida = "El rango de vida no puede ser negativo";
+    
+  }
+
 
   return error 
 }
@@ -65,79 +73,88 @@ const Create = () =>{
     }
   )   
 
-  console.log("Estos son los perros desde el create",dogs);
   useEffect(()=>{
     dispatch(get_dogs())
     dispatch(get_createdRaces())
     setError(validate(form, dogs))
   },[])
 
-
-  //estado para los temperamentos seleccionados de la lista <select /> y de los que voy a crear en el input
   const [temperaments, setTemperaments ] = useState([])
   
-  //estado del input para la creacion de nuevos temperamentos 
-  const [createTemperaments, setTCreateTemperaments ] = useState("")
-
+  console.log("array de temperamento", temperaments);
   
-
+      
   const [error,setError] = useState({}) 
+  const [errorTemps, setErrorTemps] = useState(false)
 
   //manejador de estado del form
   const handlerForm = (event) =>{
-    const {value, name} = event.target
+    const {value, name} = event.target;
+    console.log("estoy en el handler form");
 
-    if (name === "temperaments") {
-      setForm({...form, ["temperaments"] : (form.temperaments + " " + value).trim()})
-      setError(validate({...form, ["temperaments"] : form.temperaments + " " + value}, dogs))
-      return
+    if (name==="temperaments") {    
+      if(temperaments.length === 3 ) return;
+      if (temperaments.findIndex(temp => temp===value) === -1) {
+      setTemperaments([...temperaments, value])  
+      setErrorTemps(true)
     }
+    
+    return ;
+  }
+
+  if(!temperaments.length){
+    setErrorTemps(false)
+  }
 
     setForm({
       ...form,
       [name] : value
     })
+    
+    
     setError(validate({...form, [name] : value}, dogs))
   }
   const handlerSubmit = (event)=>{
     event.preventDefault() 
   }
 
-  const handlerCreateTemperament = (event) =>{
-    let value = event.target.value
-    setTCreateTemperaments(value)
+  //agrego temperamentos
+  const handlerTemperaments = ({target}) =>{ 
+    // const {value} = target;
+    // //si ya esta no lo agrego 
+    // if(temperaments.length === 3 ) return;
+    // if (temperaments.findIndex(temp => temp===value) === -1) {
+    //   setTemperaments([...temperaments, value])
+    //   return ;
+    // }
   }
 
-  const handlerTemperaments =(value)=>{
-    setForm({...form, ["temperaments"] : form.temperaments + " " + value})
-    setError(validate({...form, ["temperaments"] : form.temperaments + " " + value}, dogs))
+  //elimino temperamnetos
+  const deleteTemperament = (temp) =>{
+ 
 
-    setTCreateTemperaments("")
-  }
-   
-  const deleteTemperament = (value) =>{
-   
-    if(temperaments.length === 1){
-      setTemperaments([])
+  if(temperaments.length === 1){
+    setTemperaments([])
+    setErrorTemps(false)
       return 
     }
-    let update_Temperaments = form.temperaments.trim().split(" ").filter(t => t !== value)
-    setForm({...form,["temperaments"] : update_Temperaments.join(" ")})
-    setError(validate({...form, ["temperaments"] : update_Temperaments.join(" ")}, dogs))
+    let update_Temperaments = temperaments.filter(t => t !== temp)
+    setTemperaments(update_Temperaments)
   }  
 
-  const [stateModal, setStateModal] = useState(false)
+
+
+
  
   const handlerSendData = () =>{
-    setStateModal(true)
-    dispatch(post_dog(form))
+    let temperaments_ = temperaments.join(" ")
     setForm(   {
       name : "",
       alturaMax : "",
       alturaMin : "",
       pesoMax : "",
       pesoMin : "",
-      temperaments :"",
+      temperaments :temperaments_,
       vidaMax : "",
       vidaMin : "",
       image : "",
@@ -145,6 +162,9 @@ const Create = () =>{
     })
 
   }
+
+
+
 
  
   if(!Object.keys(dogs).length){
@@ -214,46 +234,47 @@ const Create = () =>{
                 </div>
 
                 <div className={style.temperaments}>
-                      <p>Elige los temperamentos que más identifique a tu mascota o escribelo tú.
+                      <p>Elige los temperamentos que más identifique a tu mascota.
                       Puedes poner hasta tres </p>
                   <div className={style.containerSelect} >
+
                       <div className={style.containerSelect_select}>
-                        <select  onChange={handlerForm} className={style.select} name="temperaments" id="">
-                          <option value="lista" disabled   >Lista de temperamentos</option>
+
+                        <select  onChange={(e)=>{
+                          handlerTemperaments(e)
+                          handlerForm(e)
+                        } 
+                        } className={style.select} name="temperaments" id="">
+                          <option value="lista" disabled>Lista de temperamentos</option>
                           {selectorTemps.length ? selectorTemps.map((temp)=>{
                             return <option
                             key={temp.id}
-                          
                             value = {temp.name.trim()}
                             >{temp.name.trim()}</option>
                           }): null} 
                         </select>
-                      </div>
-                      <div className={style.containerSelect_input}>
-                       
-                          <input onChange={handlerCreateTemperament}  name="search" type="text"   value={createTemperaments} /> 
-                          <button onClick={()=>handlerTemperaments(createTemperaments)} >AGREGAR</button>
-                       
-                      </div>
+                      </div>             
                     </div>
                     
 
                     <div className={style.seleccionados_container} >
                       <div className={style.seleccionados}>
-                        <p>SELECCIONADOS</p>
+                           
+                        <p id={style.error}>{errorTemps ? errorTemps :null}  </p>
                         <div>
-
-                          {form.temperaments.split(" ").map((temp)=>{
-                            if(temp === "") return
-                            return <div 
-                              className={style.temps_selected}
-                              key = {temp}>
-                                <p
-                                value ={temp}
-                                onClick = {(e)=> deleteTemperament(e.target.innerHTML)}
-                                >{temp}</p> 
-                            </div> 
-                          })}
+                          {/*aca se muestran los temperamentos selecionados */}
+                          {
+                            temperaments.map(temp =>{
+                              let count = 0
+                              return <div
+                                key={++count}                              
+                                onClick={()=>deleteTemperament(temp)}
+                                className={style.temps_selected}
+                              >
+                                {temp}
+                              </div>
+                            })
+                          }
                         </div>
                       </div>  
                     </div>
@@ -265,128 +286,12 @@ const Create = () =>{
                   <p id={style.error_button}>
                     {error.vacio ? <p>{error.vacio}</p> : null}
                   </p>
-                  <button  disabled={Object.keys(error).length} onClick={()=>{handlerSendData()}} className={style.sigin_btn} type="submit">ENVIAR</button>
-                </div>
+                  {console.log(Boolean(errorTemps))}
+                  <button  disabled={  Object.keys(error).length || !errorTemps} onClick={()=>{handlerSendData()}} className={style.sigin_btn} type="submit">ENVIAR</button>
+                </div> 
               </form>
           </div>
-          {/* {
-            Object.keys(error).length ? 
-            <div  className={style.error}>
-         
-             
-
-              {error.peso ? <p>{error.peso}</p> : null}
-              {error.pesoNegativo ? <p>{error.pesoNegativo}</p> : null}
-              {error.caracteres ? <p>{error.caracteres}</p> : null}
-
-              {error.vidaNegativa ? <p>{error.vidaNegativa}</p> : null}
-
-
-             
-            </div>
-            : null 
-          } */}
-
-{/*      
-<form class="form">
-    <div class="header">Sign In</div>
-    <div class="inputs">
-        <input placeholder="Email" class="input" type="text">
-        <input placeholder="Password" class="input" type="password">
-        <div class="checkbox-container">
-            <label class="checkbox">
-            <input type="checkbox" id="checkbox">
-            </label>
-            <label for="checkbox" class="checkbox-text">Remember me</label>
-        </div>
-        <button class="sigin-btn">Submit</button>
-        <a class="forget" href="#">Forget password ?</a>
-        <p class="signup-link">Don't have an account? <a href="#">Sign up</a></p>
-    </div>
-</form>
-
-                <div id={style.medidas}>
-                  <label htmlFor="altura">Altura</label> 
-                  <div className={style.medidas_input} >
-                    <input  type="text" placeholder="min" onChange={handlerForm}  name="alturaMin" value={form.alturaMin} />
-                    <input  type="text" placeholder="max" onChange={handlerForm}  name="alturaMax" value={form.alturaMax} />
-                  </div> 
-                </div>  
-
-                <div id={style.medidas}>
-                  <label htmlFor="peso">Peso</label>
-                  <div className={style.medidas_input} >
-                    <input  type="text" placeholder="min" onChange={handlerForm}  name="pesoMin" value={form.pesoMin} />
-                    <input  type="text" placeholder="max" onChange={handlerForm}  name="pesoMax" value={form.pesoMax} />
-                  </div>
-                </div> 
-            
-                <div id={style.medidas}>
-                  <label htmlFor="vida">Rango de vida aproximado </label>
-                  <div className={style.medidas_input} >
-                    <input  type="text" placeholder="min" onChange={handlerForm}  name="vidaMin" value={form.vidaMin} />
-                    <input  type="text" placeholder="max" onChange={handlerForm}  name="vidaMax" value={form.vidaMax} />
-                  </div>
-                </div>
-
-                <div id={ form.name && !Object.keys(error).length && style.button}  className={style.sigin_btn}>
-                  <button  disabled={ Object.keys(error).length} onClick={()=>{handlerSendData()}} type="submit">ENVIAR</button>
-                </div>
-
-              </div>
-
-              <div className={style.formTemperaments} >
-                  <div className={style.header} >
-                    <div className={style.header_p}>
-                      <p>SELECCIONA LOS TEMPERAMENTO</p> 
-                    </div>
-
-                    <div className={style.header_select} >
-                      <select  onChange={handlerForm} className={style.select} name="temperaments" id="">
-                        <option value="lista" disabled   >Lista de temperamentos</option>
-                        {selectorTemps.length ? selectorTemps.map((temp)=>{
-                          return <option
-                          key={temp.id}
-                        
-                          value = {temp.name.trim()}
-                          >{temp.name.trim()}</option>
-                        }): null} 
-                      </select>
-                    </div>
-
-
-                    <div className={style.header_input} > 
-                      <div className={style.inputs}>
-                        <input onChange={handlerCreateTemperament}  name="search" type="text"   value={createTemperaments} /> 
-                        <button onClick={()=>handlerTemperaments(createTemperaments)} >AGREGAR</button>
-                      </div > 
-                    </div>
-                  </div>
-
-                  <div className={style.seleccionados} >
-                    <p>SELECCIONADOS</p>
-                    <div>
-                      {form.temperaments.split(" ").map((temp)=>{
-                        if(temp === "") return
-                        return <div 
-                          className={style.temps_selected}
-                          key = {temp}>
-                            <p
-                            value ={temp}
-                            onClick = {(e)=> deleteTemperament(e.target.innerHTML)}
-                            >{temp}</p> 
-                        </div> 
-                      })}
-                    </div>  
-                  </div>
-              </div>
-          
-
-
-
-           */}
-          
-        </div>
+      </div>
     )
   }
 }  
